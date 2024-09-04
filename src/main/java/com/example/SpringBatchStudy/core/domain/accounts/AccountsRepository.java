c5fe5c6edfe99abd4e4b13e3df641116d9dc24db
seonghyeoncho <chosh040@naver.com>
2024-09-04 23:28:21 +0900

    :truck: [rename] java, kotlin 분리를 위한 폴더 생성 및 이전
    Resolves:
    Ref:
    Related to:

--- src/main/java/com/example/SpringBatchStudy/core/domain/accounts/AccountsRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.SpringBatchStudy.core.domain.accounts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountsRepository extends JpaRepository<Accounts, Integer> {}
--- src/main/java/com/example/SpringBatchStudy/core/domain/orders/OrdersRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.SpringBatchStudy.core.domain.orders;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrdersRepository extends JpaRepository<Orders, Integer> {}
